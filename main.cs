f099262d05aa659b846c7cc59fbf253e99138165
vadim-221212 <vdmed@bk.ru>
2023-10-01 21:27:56 +0300

    work

---
+++ main.cs
@@ -0,0 +1,22 @@
+using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine; 
+ 
+public class NewBehaviourScript : MonoBehaviour 
+{ 
+    public Color newColor; 
+    public float speed; 
+ 
+    private Renderer rend; 
+ 
+    void Start() 
+    { 
+        rend = GetComponent<Renderer>(); 
+    } 
+ 
+    void Update() 
+    { 
+        rend.material.color = newColor; 
+        transform.Translate(Vector3.forward * speed * Time.deltaTime); 
+    } 
+}
